b32cd14d6270b1d1c20f79e6b17a4d104fd3cc05
pra17dod <pra17dod@gmail.com>
2021-02-15 00:43:46 +0530

    Add frontent

--- deploy/server/tryon/views.py
+++ deploy/server/tryon/views.py
@@ -1,16 +1,11 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
 from django.template import Template
 from tryon.models import Tryon
 from tryon.forms import TryonForm
-from PIL import Image
-import sys
 import os
-import cv2
-import base64
-from django.template import RequestContext
-
+import sys
 sys.path.append(os.path.abspath('model'))
 from virtuon import virtuon
 from clear import clear
@@ -20,11 +15,12 @@ from pairs import pairs
 
 class TryonView(CreateView):
     model = Tryon
-    template = "home.html"
+    template = "index.html"
     success_url = "predict.html"
 
 
     def get(self, request):
+        clear()
         form = TryonForm()
         ctx = {'form': form}
         return render(request, self.template, ctx)
@@ -34,7 +30,6 @@ class TryonView(CreateView):
         if not form.is_valid():
             ctx = {'form': form}
             return render(request, self.template, ctx)
-
         form.save()
         return render(request, self.template)
 
@@ -42,20 +37,9 @@ class TryonPredict(ListView):
     template = "predict.html"
 
     def get(self, request):
-        clear()
         pairs()
         virtuon()
-        # # base_image = Image.open("model/output/p_rendered/demo_1.jpg")
-        # # response = HttpResponse(content_type="image/png")
-        # # base_image.save(response, "PNG")
         output = ("output/d0.jpg")
-        # template = Template('{{MEDIA_URL}}:{{output}}')
         ctx = {"output": output}
-         # img_path = ('model/output/p_rendered/demo_1.jpg')
-        # # img = cv2.imread(img_path)
-        # # _, img_encoded = cv2.imencode('.jpg', img)
-        # # ctx = {"output": image}
-        # # return render(request, self.template, ctx)
-        # ctx = RequestContext(request, { 'output': output })
 
         return render(request, self.template, ctx)
