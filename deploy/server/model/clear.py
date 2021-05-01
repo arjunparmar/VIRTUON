60c0fb59d8069f72d4f34e8586721d7014900125
pra17dod <pra17dod@gmail.com>
2021-05-01 22:59:10 +0530

    Add models

---
+++ deploy/server/model/clear.py
@@ -0,0 +1,108 @@
+'''
+import os
+import glob
+import shutil
+
+def clear():
+    img_mask = glob.glob(os.path.abspath('model/input/test/test/image-mask/*'))
+    cloth_mask = glob.glob(os.path.abspath('model/input/test/test/cloth-mask/*'))
+    img_parse = glob.glob(os.path.abspath('model/input/test/test/image-parse/*'))
+    img_parse_new = glob.glob(os.path.abspath('model/input/test/test/image-parse-new/*'))
+    overlay = glob.glob(os.path.abspath('model/input/test/test/overlayed-TPS/*'))
+    pose = glob.glob(os.path.abspath('model/input/test/test/pose/*'))
+    gmm_result = glob.glob(os.path.abspath('model/input/test/test/result-dir/*'))
+    warp_cloth = glob.glob(os.path.abspath('model/input/test/test/warp-cloth/*'))
+    warp_mask = glob.glob(os.path.abspath('model/input/test/test/warp-mask/*'))
+
+    input_image = glob.glob(os.path.abspath('model/input/test/test/image/*'))
+    input_cloth = glob.glob(os.path.abspath('model/input/test/test/cloth/*'))
+
+    try:
+        shutil.rmtree('./media/image')
+        # shutil.rmtree('./media/cloth')
+        # shutil.rmtree('./media/output')
+    except:
+        pass
+    try:
+        # shutil.rmtree('./media/image')
+        shutil.rmtree('./media/cloth')
+        # shutil.rmtree('./media/output')
+    except:
+        pass
+    try:
+        # shutil.rmtree('./media/image')
+        # shutil.rmtree('./media/cloth')
+        shutil.rmtree('./media/output')
+    except:
+        pass
+
+    for i in range(len(img_mask)):
+        try:
+            os.remove(input_image[i])
+        except:
+            pass
+        try:
+            os.remove(input_cloth[i])
+        except:
+            pass
+        try:
+            os.remove(img_mask[i])
+        except:
+            pass
+        try:
+            os.remove(cloth_mask[i])
+        except:
+            pass
+        try:
+            os.remove(img_parse[i])
+        except:
+            pass
+        try:
+            os.remove(img_parse_new[i])
+        except:
+            pass
+        try:
+            os.remove(overlay[i])
+        except:
+            pass
+        try:
+            os.remove(pose[i])
+        except:
+            pass
+        try:
+            os.remove(gmm_result[i])
+        except:
+            pass
+        try:
+            os.remove(warp_cloth[i])
+        except:
+            pass
+        try:
+            os.remove(warp_mask[i])
+        except:
+            pass
+'''
+import glob
+import os
+import sys
+
+def clear():
+	
+	base_dir = os.path.abspath('media')
+	image_dir = os.path.join(base_dir, 'image')
+	cloth_dir = os.path.join(base_dir, 'cloth')
+	output_dir = os.path.join(base_dir, 'output')
+	
+	
+	for i in os.listdir(image_dir):
+		os.remove(os.path.join(image_dir, i))
+	
+	for i in os.listdir(cloth_dir):
+		os.remove(os.path.join(cloth_dir, i))
+	
+	for i in os.listdir(output_dir):
+		os.remove(os.path.join(output_dir, i))
+	
+
+if __name__ == '__main__':
+    clear()
---
+++ deploy/server/model/virtuon/image_resize.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+from PIL import Image
+import torch
+import torchvision.transforms as transforms
+import os
+def image_resize():
+	base_dir_1 = os.path.abspath('./model/input/test/test/image')
+	base_dir_2 = os.path.abspath('./model/input/test/test/cloth')
+	# print(base_dir)
+	image_list = os.listdir(base_dir_1)
+	cloth_list = os.listdir(base_dir_2)
+	transformation = transforms.Compose([transforms.Resize((256,192), interpolation = Image.BICUBIC)])
+	
+	for i in image_list:
+		temp = Image.open(os.path.join(base_dir_1, i))
+		# temp = cv.resize(temp, (192,256))
+		temp = transformation(temp)
+		temp.save(os.path.join(base_dir_1, i))
+
+	for i in cloth_list:
+		temp = Image.open(os.path.join(base_dir_2, i))
+		# temp = cv.resize(temp, (192,256))
+		temp = transformation(temp)
+		temp.save(os.path.join(base_dir_2, i))
+
