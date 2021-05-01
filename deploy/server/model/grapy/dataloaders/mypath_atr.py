887d17184c9182ee6ccfb81b0760542761ef128f
pra17dod <pra17dod@gmail.com>
2021-05-01 22:56:45 +0530

    Formatting files as per gitignore

--- deploy/server/model/grapy/dataloaders/mypath_atr.py
+++
@@ -1,8 +0,0 @@
-class Path(object):
-    @staticmethod
-    def db_root_dir(database):
-        if database == 'atr':
-            return './data/datasets/ATR/'  # folder that contains atr/.
-        else:
-            print('Database {} not available.'.format(database))
-            raise NotImplementedError
--- deploy/server/tryon/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TryonConfig(AppConfig):
-    name = 'tryon'
