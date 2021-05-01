18a7b1b64eda299a597a9019a7fab07f9178cf44
pra17dod <pra17dod@gmail.com>
2021-05-02 02:31:52 +0530

    pairs.py: Add path condition

--- deploy/server/model/pairs.py
+++ deploy/server/model/pairs.py
@@ -4,6 +4,8 @@ from shutil import copy2
 def pairs():
     base_dir_1 = os.path.abspath('media')
     base_dir_2 = os.path.abspath('model/input/test/test/')
+    if not os.path.exists(base_dir_2):
+    	os.makedirs(base_dir_2)
     img_dir_1 = os.path.join(base_dir_1, 'image')
     cloth_dir_1 = os.path.join(base_dir_1, 'cloth')
     img_dir_2 = os.path.join(base_dir_2, 'image')
