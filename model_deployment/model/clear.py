f273b3cf1fc93db4ab3bc69c27232ff4069d60ff
pra17dod <pra17dod@gmail.com>
2021-05-23 03:10:35 +0530

    clear.py: Clear images from pose-vis

--- model_deployment/model/clear.py
+++ model_deployment/model/clear.py
@@ -8,6 +8,7 @@ def clear():
 	image_dir = os.path.join(base_dir, 'image')
 	cloth_dir = os.path.join(base_dir, 'cloth')
 	output_dir = os.path.join(base_dir, 'output')
+	pose_dir = os.path.join(base_dir, 'pose-vis')
 
 	for i in os.listdir(image_dir):
 		os.remove(os.path.join(image_dir, i))
@@ -17,6 +18,9 @@ def clear():
 
 	for i in os.listdir(output_dir):
 		os.remove(os.path.join(output_dir, i))
+
+	for i in os.listdir(pose_dir):
+		os.remove(os.path.join(pose_dir, i))
 
 if __name__ == '__main__':
     clear()
