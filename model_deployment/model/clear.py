720494dd1fbb6e1560a0104f2000184ed98fe45e
H4R5H1T-007 <hgupta1153@gmail.com>
2021-05-26 17:14:40 +0530

    Adding exist condition
    Checking file path and if not exists file path
    then we create them.

--- model_deployment/model/clear.py
+++ model_deployment/model/clear.py
@@ -2,6 +2,10 @@ import glob
 import os
 import sys
 
+def mkdir(path):
+	if not os.path.exists(path):
+		os.makedirs(path)
+
 def clear():
 
 	base_dir = os.path.abspath('media')
@@ -9,12 +13,17 @@ def clear():
 	cloth_dir = os.path.join(base_dir, 'cloth')
 	output_dir = os.path.join(base_dir, 'output')
 
+	mkdir(base_dir)
+
+	mkdir(image_dir)
 	for i in os.listdir(image_dir):
 		os.remove(os.path.join(image_dir, i))
 
+	mkdir(cloth_dir)
 	for i in os.listdir(cloth_dir):
 		os.remove(os.path.join(cloth_dir, i))
 
+	mkdir(output_dir)
 	for i in os.listdir(output_dir):
 		os.remove(os.path.join(output_dir, i))
 
